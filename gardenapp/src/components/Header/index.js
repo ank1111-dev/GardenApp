c8de8bddde1837a38e759f6e5a0387653b606575
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-14 16:26:40 +1000

    created a header component

---
+++ gardenapp/src/components/Header/index.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import { List, ListItemButton } from '@mui/material';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Gardening App for Australia</h1>
+      <nav>
+        <List>
+          <ListItemButton component={Link} to="/">
+            Home
+          </ListItemButton>
+        </List>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
