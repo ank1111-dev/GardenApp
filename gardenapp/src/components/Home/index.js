444a5b919d874243451a15a42de666f13a3d8591
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-14 16:28:49 +1000

    created a home component

---
+++ gardenapp/src/components/Home/index.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to gardening app!</p>
+      <p>
+        This app helps you find the best plants to grow in your garden based on
+        your location and the weather.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
