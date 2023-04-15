83d6b39c636f731512ae0335e7a683fc6f69b005
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-15 21:42:20 +1000

    Added a image and changed the css for home page

--- gardenapp/src/components/Home/index.js
+++ gardenapp/src/components/Home/index.js
@@ -1,14 +1,19 @@
+import { Box, Typography } from '@mui/material';
+import gardenImage from './image/garden.jpg'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to gardening app!</p>
-      <p>
-        This app helps you find the best plants to grow in your garden based on
-        your location and the weather.
-      </p>
-    </div>
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh'   }}>
+      <Box sx={{ maxWidth: '600px', textAlign: 'center', bgcolor: '#F5F5F5'  }}>
+        <Typography variant="h4" sx={{ mb: 6, mt: 4 }}>Welcome to Gardening App!</Typography>
+        <Typography variant="body1" sx={{ mb: 4 }}>
+          Our app helps you find the best plants to grow in your garden based on your location and the weather. With our expert suggestions and personalized recommendations, you'll have a thriving garden in no time!
+        </Typography>
+        <img src={gardenImage} alt="Garden" style={{ maxWidth: '100%' }} />
+      </Box>
+    </Box>
   );
 };
 
 export default Home;
+
