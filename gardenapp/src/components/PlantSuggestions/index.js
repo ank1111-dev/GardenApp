a5795346ea8d57f232d8e86cc4bc032385b02395
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-15 17:37:53 +1000

    Added a grid to display plant names in list form.

--- gardenapp/src/components/PlantSuggestions/index.js
+++ gardenapp/src/components/PlantSuggestions/index.js
@@ -1,24 +1,51 @@
+import { useState } from 'react';
 import { data } from '../PlantData';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Grid, Typography } from '@mui/material';
 
 const PlantSuggestions = ({ climateZone }) => {
-  const plants = data.filter(plant => plant.climate_zones.includes(climateZone));
+  const [selectedPlant, setSelectedPlant] = useState(null);
+  const plants = data.filter((plant) => plant.climate_zones.includes(climateZone));
+
+  const handlePlantClick = (plant) => {
+    setSelectedPlant(plant);
+  };
 
   return (
     <div>
       <Typography variant="h4">Suggested Plants for {climateZone} Climate Zone</Typography>
-      {plants.map(plant => (
-        <Card key={plant.id} sx={{ mt: 2 }}>
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        {plants.map((plant) => (
+          <Grid item key={plant.id} xs={6} sm={4} md={3}>
+            <Card onClick={() => handlePlantClick(plant)}>
+              <CardContent>
+                <Typography variant="h6">{plant.name}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      {selectedPlant && (
+        <Card sx={{ mt: 2 }}>
           <CardContent>
-            <Typography variant="h5">{plant.name}</Typography>
-            <Typography variant="body1">{plant.description}</Typography>
-            <Typography variant="body2"><strong>Ideal Growing Conditions: </strong>{plant.ideal_growing_conditions}</Typography>
-            <Typography variant="body2"><strong>Care: </strong>{plant.care}</Typography>
+            <Typography variant="h5">{selectedPlant.name}</Typography>
+            <Typography variant="body1">{selectedPlant.description}</Typography>
+            <Typography variant="body2">
+              <strong>Ideal Growing Conditions: </strong>
+              {selectedPlant.ideal_growing_conditions}
+            </Typography>
+            <Typography variant="body2">
+              <strong>Care: </strong>
+              {selectedPlant.care}
+            </Typography>
           </CardContent>
         </Card>
-      ))}
+      )}
     </div>
   );
 };
 
 export default PlantSuggestions;
+
+
+
+
