fb3f47c90ce46e1e861171dfe2909cbefdf76c91
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-15 21:16:24 +1000

    add header image and css

--- gardenapp/src/components/Header/index.js
+++ gardenapp/src/components/Header/index.js
@@ -1,22 +1,34 @@
 import { Link } from 'react-router-dom';
-import { List, ListItemButton } from '@mui/material';
+import { Box, List, ListItemButton } from '@mui/material';
+import headerImage from './image/header.jpg';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Gardening App for Australia</h1>
-      <nav>
-        <List>
-          <ListItemButton component={Link} to="/">
-            Home
-          </ListItemButton>
-          <ListItemButton component={Link} to="/suggestions">
-            Gardening Suggestions
-          </ListItemButton>
-        </List>
-      </nav>
-    </header>
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', bgcolor: '#ffffff', flexDirection: 'column' }}>
+      <Box sx={{ flex: '1 1 auto',  width: '100%', margin: 0}}>
+        <img src={headerImage} alt="" style={{ width: '100%', height: '300px' }}/>
+      </Box>
+      <Box sx={{ flex: '2 1 auto', textAlign: 'center' }}>
+        <h1 style={{ margin: 0, fontWeight: 'bold' }}>Aussie Garden Wizard</h1>
+      </Box>
+      <Box sx={{ flex: '1 1 auto' }}>
+        <nav>
+          <List sx={{ display: 'flex', justifyContent: 'center' }}>
+            <ListItemButton component={Link} to="/" sx={{ mx: 2 }}>
+              Home
+            </ListItemButton>
+            <ListItemButton component={Link} to="/suggestions" sx={{ mx: 2 }}>
+              Gardening Suggestions
+            </ListItemButton>
+          </List>
+        </nav>
+      </Box>
+    </Box>
   );
 };
 
 export default Header;
+
+
+
+
