7e34de11bea6877d6e910b2d7dc29e1d416fb0a2
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-16 16:14:04 +1000

    Added styling and animation to card

--- gardenapp/src/components/PlantSuggestions/index.js
+++ gardenapp/src/components/PlantSuggestions/index.js
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { data } from '../PlantData'
 import { Card, CardContent, Grid, Typography } from '@mui/material'
+import { AnimatePresence, motion } from "framer-motion"
 
 const PlantCard = ({ plant, onClick, isSelected }) => {
   return (
-    <Card onClick={onClick} sx={{ mb: 2, bgcolor: isSelected ? 'primary.main' : 'transparent' }}>
+    <Card onClick={onClick} sx={{ mb: 2, bgcolor: isSelected ? 'primary.main' : 'transparent', borderRadius: 4, boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.15)', color: isSelected ? 'common' : 'initial' , backgroundColor: "#dcd0ff"}}>
       <CardContent>
-        <Typography variant="h6" textAlign="center" sx={{ color: isSelected ? 'common.white' : 'initial' }}>{plant.name}</Typography>
+        <Typography variant="h6" textAlign="center" sx={{ fontWeight: 'bold', color: "#8a2be2" }}>{plant.name}</Typography>
         {isSelected && (
           <>
             <Typography variant="body1">{plant.description}</Typography>
@@ -34,32 +35,68 @@ const PlantCard = ({ plant, onClick, isSelected }) => {
 };
 
 const PlantSuggestions = ({ climateZone }) => {
-  const [selectedPlant, setSelectedPlant] = useState(null);
-  const plants = data.filter((plant) => plant.climate_zones.includes(climateZone))
-
+  const [selectedPlant, setSelectedPlant] = useState(null)
+  const [showMore, setShowMore] = useState(false)
+  const plantsToShow = showMore ? data.filter((plant) => plant.climate_zones.includes(climateZone)) : data.filter((plant) => plant.climate_zones.includes(climateZone)).slice(0, 6)
+  const plantsToHide = showMore ? [] : data.filter((plant) => plant.climate_zones.includes(climateZone)).slice(6)
   const handlePlantClick = (plant) => {
     if (selectedPlant === plant) {
       setSelectedPlant(null)
     } else {
       setSelectedPlant(plant)
     }
+  }
+
+  const handleLoadMoreClick = () => {
+    setShowMore(true);
+  };
+  const HappyGardening = () => {
+    const [colorIndex, setColorIndex] = useState(0);
+    const colors = ["#98fb98", "#00fa9a", "#9932cc", "#6a5acd"];
+    
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+      }, 500);
+      return () => clearInterval(intervalId);
+    }, [colors.length]);
+  
+    return (
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Typography variant="h4" sx={{ color: colors[colorIndex] }}>Happy Gardening!!</Typography>
+        </motion.div>
+      </AnimatePresence>
+    );
   };
 
   return (
-    <div>
-      <Typography variant="h4">Suggested Plants for {climateZone} Climate Zone</Typography>
+    <>
+      {/* <Typography variant="h4" sx={{color: "#4b0082"}} >Happy Gardening!!</Typography> */}
+      <HappyGardening />
       <Grid container spacing={2} sx={{ mt: 2 }}>
-        {plants.map((plant) => (
+        {plantsToShow.map((plant) => (
+          <Grid item key={plant.id} xs={6} sm={4} md={3}>
+            <PlantCard plant={plant} onClick={() => handlePlantClick(plant)} isSelected={selectedPlant === plant} />
+          </Grid>
+        ))}
+        {!showMore && (
+          <Grid item xs={12} sx={{ textAlign: 'center', mt: 2 }}>
+            <Typography variant="button" onClick={handleLoadMoreClick} sx={{ cursor: 'pointer', textDecoration: 'underline' }}>Load More Plants</Typography>
+          </Grid>
+        )}
+        {showMore && plantsToHide.map((plant) => (
           <Grid item key={plant.id} xs={6} sm={4} md={3}>
             <PlantCard plant={plant} onClick={() => handlePlantClick(plant)} isSelected={selectedPlant === plant} />
           </Grid>
         ))}
       </Grid>
-    </div>
+    </>
   );
 };
 
 export default PlantSuggestions
-
-
-
