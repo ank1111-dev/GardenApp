d9a95dd30a029ddd78e6d2189b5310bcb8890e74
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-17 00:50:42 +1000

    added semantics and changed the styling

--- gardenapp/src/components/Articles/index.js
+++ gardenapp/src/components/Articles/index.js
@@ -1,17 +1,17 @@
 import { gardeningArticles } from './gardening-articles';
-import { Card, CardActionArea, CardMedia, CardContent, Typography, List, ListItem } from '@mui/material';
+import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Articles = () => {
   return (
-    <div style={{ maxWidth: '800px', margin: '0 auto' }}>
-      <Typography variant="h4" sx={{ textAlign: 'center', margin: '2rem 0' }}>
+    <div style={{ height: '100vh'}}>
+      <Typography variant="h4" align="center" gutterBottom style={{  marginTop: '30px', marginBottom: '40px', color: '#b6986d'}}>
         Gardening Articles
       </Typography>
-      <List sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap' }}>
+      <Grid container spacing={4} justifyContent="center">
         {gardeningArticles.map((article, index) => (
-          <ListItem key={index} sx={{ margin: '1rem', padding: 0, width: '300px' }}>
-            <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }} component={Link} to={article.url} target="_blank">
+          <Grid item key={index} xs={12} sm={6} md={4}>
+            <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' , color: '#9C27B0',  '&:hover': { color: '#78c677'} }} component={Link} to={article.url} target="_blank">
               <CardActionArea sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
                 <CardMedia
                   component="img"
@@ -20,20 +20,17 @@ const Articles = () => {
                   sx={{ height: '150px', objectFit: 'cover' }}
                 />
                 <CardContent sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: 'center' }}>
+                  <Typography gutterBottom variant="h5" component="div" align="center">
                     {article.title}
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
-          </ListItem>
+          </Grid>
         ))}
-      </List>
+      </Grid>
     </div>
   );
 };
 
-export default Articles;
-
-
-
+export default Articles
