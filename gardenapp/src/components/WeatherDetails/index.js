6dcfbdeda93decbef2ab25af4cdc2650b2e3f9b6
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-16 15:23:22 +1000

    add style and css in weather details

--- gardenapp/src/components/WeatherDetails/index.js
+++ gardenapp/src/components/WeatherDetails/index.js
@@ -1,8 +1,63 @@
 import React, { useEffect, useState, useCallback } from 'react'
 import { useParams } from 'react-router-dom';
-import { CircularProgress, Typography } from '@mui/material'
+import { CircularProgress, Typography, styled } from '@mui/material'
 import PlantSuggestions from '../PlantSuggestions'
 import {data} from '../PlantData'
+
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  backgroundColor: '#f0f0f0',
+});
+
+const Title = styled(Typography)({
+  fontSize: '2rem',
+  marginBottom: '10px',
+});
+
+const DateTypography = styled(Typography)({
+  fontSize: '1rem',
+  marginBottom: '20px',
+});
+
+const WeatherBox = styled('div')({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+  gap: '20px',
+  alignItems: 'center',
+  justifyItems: 'center',
+  padding: '20px',
+  backgroundColor: '#fff',
+  borderRadius: '10px',
+  boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+});
+
+const WeatherIcon = styled('img')({
+  marginTop: '10px',
+  animation: `spin 2s linear infinite`,
+  '@keyframes spin': {
+    from: { transform: 'rotate(0deg)' },
+    to: { transform: 'rotate(360deg)' },
+}});
+
+const WeatherDetailsContainer = styled('div')({
+  margin: '20px 0',
+});
+
+const Weather = styled('div')({
+    display: 'flex',
+    alignItems: 'center',
+})
+
+
+const SuggestionContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '20px',
+});
 
 const WeatherDetails = () => {
   const { location } = useParams();
@@ -11,6 +66,7 @@ const WeatherDetails = () => {
   const apiKey = process.env.REACT_APP_WEATHER_API_KEY
 
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
+  
 
   const fetchWeatherData = useCallback(() => {
     fetch(apiUrl)
@@ -68,24 +124,45 @@ const WeatherDetails = () => {
   }
 
   console.log(weatherData); 
+  const weatherIconUrl = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
 
   const temperature = Math.round(weatherData.main.temp - 273.15);
   const weatherDescription = weatherData.weather[0].description; 
+  const currentDate = new Date();
+  const dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
+  const date = currentDate.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+  
 
   return (
+    <Container>
+    {!weatherData && <CircularProgress />}
+    {weatherData && (
     <>
-      <h2>Weather Details</h2>
-      <p>Temperature: {temperature}°C</p>
-      <p>Weather: {weatherDescription}</p>
-      {suggestedPlants.length > 0 ? (
-        <PlantSuggestions climateZone={getClimateZone(temperature)} />
-      ) : (
-        <Typography variant="h4">No suggested plants for this climate zone.</Typography>
-      )}
-    </>
-  );
-}  
-
+    <Title>{weatherData.name}</Title>
+    <DateTypography>{date}, {dayOfWeek}</DateTypography>    
+    <WeatherBox>
+    <WeatherDetailsContainer>
+    <Typography variant="h6">Temperature: {temperature}°C</Typography>
+    <Weather>    
+    <Typography variant="h6">Description: {weatherDescription} <span></span></Typography>
+    <WeatherIcon src={weatherIconUrl} alt="Weather Icon" />
+    </Weather>
+    <Typography variant="h6">Humidity: {weatherData.main.humidity}%</Typography>
+    <Typography variant="h6">Wind Speed: {weatherData.wind.speed} m/s</Typography>
+    </WeatherDetailsContainer>
+    </WeatherBox>
+    {suggestedPlants.length > 0 ? (
+  <SuggestionContainer>
+    <PlantSuggestions climateZone={getClimateZone(temperature)} />
+  </SuggestionContainer>
+  ) : (
+  <Typography variant="h4">No suggested plants for this climate zone.</Typography>
+  )}   
+  </>
+    )}
+    </Container>
+    )
+    }
 export default WeatherDetails;
 
 
