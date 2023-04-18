9950cb43f400f9f60b23af225163cc8658ec6c13
ank1111-dev <62601460+ank1111-dev@users.noreply.github.com>
2023-04-18 18:12:47 +1000

    Added icons and changed the styling

--- gardenapp/src/components/GardeningWizard/index.js
+++ gardenapp/src/components/GardeningWizard/index.js
@@ -1,21 +1,31 @@
 import { Link } from 'react-router-dom';
 import { Card, CardContent, CardHeader } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import DescriptionIcon from '@mui/icons-material/Description';
 
 const GardeningWizard = () => {
   return (
-    <div  style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' , backgroundColor:"#f0f0f0", height: '50vh'}}>
-      <Card style={{ margin: '100px 0',  height: '20vh' , textAlign: 'center', }}>
-        <CardHeader style={{ color: '#4CAF50'}} title="Plant Finder" />
-        <CardContent>
-          <Link to="/gardening-wizard/plant-finder" underline="hover" color="inherit">
-            Find the perfect plant for your garden
-            </Link>   
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '50px', height: '50vh' }}>
+      <Card style={{ margin: '50px', width: '200px', height: '20vh', textAlign: 'center', backgroundColor: "#f0f0f0" }}>
+        <CardHeader
+          style={{ color: '#4CAF50' }}
+          title="Plant Finder"
+        />
+        <CardContent sx={{p: 1 }}>
+          <Link to="/gardening-wizard/plant-finder" >
+            <SearchIcon style={{ fontSize: '60px', color: '#9C27B0', }} />
+          </Link>
         </CardContent>
       </Card>
-      <Card style={{ margin: '100px',  height: '20vh', textAlign: 'center'}}>
-        <CardHeader style={{ color: '#4CAF50'}} title="Articles" />
-        <CardContent>
-           <Link to="/gardening-wizard/articles">Read helpful gardening articles</Link>         
+      <Card style={{ margin: '50px', width: '200px', height: '20vh', textAlign: 'center', backgroundColor: "#f0f0f0" }}>
+        <CardHeader
+          style={{ color: '#4CAF50' }}
+          title="Articles"
+        />
+        <CardContent sx={{p: 1 }}>
+          <Link to="/gardening-wizard/articles">
+            <DescriptionIcon style={{ fontSize: '48px', color: '#9C27B0' }} />
+          </Link>
         </CardContent>
       </Card>
     </div>
